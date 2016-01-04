b5e0a0311669bf98875327e2ad0e8f60cc9a60cf
Chris1994 <ct.truebig@gmail.com>
2016-01-04 15:08:37 +1000

    Guten Translator
    Translates english to german and vice versa

---
+++ GutenTranslator.java/src/com/guttranslate/GutTranslator.java
@@ -0,0 +1,124 @@
+package com.guttranslate;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.*;
+
+class GutTranslator implements ActionListener {
+
+	JTextField jtf1;
+	JTextField jtf2;
+	JLabel jlab;
+	TransLateDao engDict = new TransLateDao("english.txt");
+	TransLateDao gerDict = new TransLateDao("german.txt");
+
+	// initialize the translator
+	public GutTranslator() {
+
+		// Create a new JFrame container.
+		JFrame frame = new JFrame("GutTranslator 1.1");
+		JPanel panel = new JPanel();
+
+		// Set size of panel.
+
+		panel.setLayout(new GridLayout());
+		// Set panel color
+		panel.setBackground(Color.LIGHT_GRAY);
+
+		// Set icon
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/textEdit.png")));
+
+		// Specify layout.
+		// frame.setLayout(new FlowLayout());
+
+		// initial frame size.
+		frame.setSize(360, 95);
+
+		// Terminate the program.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Create an empty label.
+		jlab = new JLabel("Enter any word");
+		jlab.setForeground(Color.DARK_GRAY);
+		jlab.setHorizontalAlignment(JLabel.CENTER);
+
+		// Add the label to the frame.
+		panel.add(jlab);
+
+		// Create two text field instances.
+		jtf1 = new JTextField(20);
+		jtf1.setHorizontalAlignment(JTextField.CENTER);
+		jtf2 = new JTextField(20);
+		jtf2.setHorizontalAlignment(JTextField.CENTER);
+		jtf2.setEditable(false);
+
+		// set background color.
+		jtf1.setBackground(Color.GRAY);
+		jtf2.setBackground(Color.DARK_GRAY);
+
+		// Set the borders.
+		jtf1.setBorder(null);
+		jtf2.setBorder(null);
+
+		// set foreground color.
+		jtf1.setForeground(Color.WHITE);
+		jtf2.setForeground(Color.LIGHT_GRAY);
+
+		// Set action commands.
+		jtf1.setActionCommand("One");
+		jtf2.setActionCommand("Two");
+
+		// Add action listeners
+		jtf1.addActionListener(this);
+		jtf2.addActionListener(this);
+
+		// Add to the content pane.
+		panel.add(jtf1);
+		panel.add(jtf2);
+
+		// Add panel to the frame.
+		frame.add(panel);
+
+		// Display the frame.
+		frame.setVisible(true);
+		frame.setResizable(false);
+
+	}
+
+	// Handle the events.
+	public void actionPerformed(ActionEvent ae) {
+
+		if (ae.getActionCommand().equals("One")) {
+			String text = jtf1.getText();
+
+			if (text.equals("clear")) {
+				jtf1.setText("");
+				jtf2.setText("");
+			} 
+			else if (engDict.hasWord(text)) {
+				int index = engDict.getIndex(text);
+				jtf2.setText(gerDict.getWord(index));
+			} 
+			else if (gerDict.hasWord(text)) {
+				int index = gerDict.getIndex(text);
+				jtf2.setText(engDict.getWord(index));
+			} 
+			else {
+				jtf2.setText("Check your \nspelling :(");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		// Create the frame on the event dispatching thread.
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new GutTranslator();
+			}
+		});
+	}
+}
---
+++ GutenTranslator.java/src/com/guttranslate/TransLateDao.java
@@ -0,0 +1,67 @@
+package com.guttranslate;
+
+import java.io.*;
+import java.util.ArrayList;
+
+class TransLateDao {
+
+	private ArrayList<String> language = new ArrayList<>();
+	private int counter = 0;
+	private String filePath = "C:\\Users\\Christopher\\Desktop\\Desktop Stuff\\Java Apps\\";
+
+	TransLateDao(String file) {
+		// The name of the file to open.
+		String fileName = filePath + file;
+
+		// This will reference one line at a time.
+		String line = null;
+
+		try {
+			// FileReader reads a text files in the default encoding.
+			FileReader fileReader = new FileReader(fileName);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader buffReader = new BufferedReader(fileReader);
+
+			// Load words from file to the ArrayList
+			while ((line = buffReader.readLine()) != null) {
+				System.out.println(line);
+				addWords(line);
+				counter++;
+			}
+			System.out.println(counter);
+			// Always close files.
+			buffReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch (IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+		}
+	}
+
+	void addWords(String word) {
+		language.add(word);
+	}
+
+	int getIndex(String word) {
+		int index = 0;
+		if (language.contains(word)) {
+			index = language.indexOf(word);
+		}
+		return index;
+	}
+
+	String getWord(int index) {
+		return language.get(index);
+
+	}
+
+	boolean hasWord(String word) {
+		if (language.contains(word)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
